9c134beaa0a4648edaf546408e8eb1ef54c56019
Sam Goddard <hello@samgoddard.co.uk>
2022-10-25 16:32:00 +0100

    coded supergraphic

--- pages/index.js
+++ pages/index.js
@@ -35,27 +35,23 @@ export default function Home() {
 
               {/* Right blob */}
               <MouseParallax enableOnTouchDevice={false} isAbsolutelyPositioned lerpEase={0.05} strength={0.1}>
-                <ScrollParallax enableOnTouchDevice={false} isAbsolutelyPositioned lerpEase={0.15} strength={-0.1}>
-                  <div className="absolute top-0 right-0 w-[120vw] h-[140%] rounded-full bg-gradient-to-tr from-orange via-yellow  to-yellow translate-x-[48vw] translate-y-[-32%] blur-xl overflow-hidden flex items-start justify-start scale-[1.15]">
-                    <div className="w-[45%] h-[46%] rounded-full radial mt-[7%] ml-[0%]"></div>
-                    <div className="absolute bottom-0 right-[20%] w-[50%] h-[40%] rounded-full bg-gradient-to-t from-purple via-purple/50 to-transparent rotate-[-50deg] z-[2]"></div>
-                    <div className="absolute bottom-[-10%] left-[-10%] right-0 w-[100%] h-[45%] bg-gradient-to-t from-orange to-transparent rotate-[25deg]"></div>
+                <div className="absolute top-0 right-0 w-[120vw] h-[140%] rounded-full bg-gradient-to-tr from-orange via-yellow  to-yellow translate-x-[48vw] translate-y-[-32%] blur-xl overflow-hidden flex items-start justify-start scale-[1.15]">
+                  <div className="w-[45%] h-[46%] rounded-full radial mt-[7%] ml-[0%]"></div>
+                  <div className="absolute bottom-0 right-[20%] w-[50%] h-[40%] rounded-full bg-gradient-to-t from-purple via-purple/50 to-transparent rotate-[-50deg] z-[2]"></div>
+                  <div className="absolute bottom-[-10%] left-[-10%] right-0 w-[100%] h-[45%] bg-gradient-to-t from-orange to-transparent rotate-[25deg]"></div>
 
-                    <div className="absolute bottom-[-10%] left-[24%] right-0 w-[25%] h-[45%] bg-gradient-to-t from-yellow to-yellow z-1 rounded-xl opacity-25 rotate-[-13deg]"></div>
+                  <div className="absolute bottom-[-10%] left-[24%] right-0 w-[25%] h-[45%] bg-gradient-to-t from-yellow to-yellow z-1 rounded-xl opacity-25 rotate-[-13deg]"></div>
 
-                    <div className="absolute bottom-[-25%] left-[-10%] right-0 w-[70%] h-[45%] bg-gradient-to-t from-purple to-transparent rotate-[25deg] z-1"></div>
-                  </div>
-                </ScrollParallax>
+                  <div className="absolute bottom-[-25%] left-[-10%] right-0 w-[70%] h-[45%] bg-gradient-to-t from-purple to-transparent rotate-[25deg] z-1"></div>
+                </div>
               </MouseParallax>
 
               {/* Left blob */}
               <MouseParallax enableOnTouchDevice={false} isAbsolutelyPositioned lerpEase={0.05} strength={-0.1}>
-                <ScrollParallax enableOnTouchDevice={false} isAbsolutelyPositioned lerpEase={0.15} strength={-0.15}>
-                  <div className="absolute top-0 left-0 w-[70vw] h-[130%] rounded-full bg-gradient-to-t from-orange via-yellow to-yellow translate-x-[-50vw] translate-y-[-15%] blur-xl flex items-center justify-center overflow-hidden">
-                    <div className="w-[80%] h-[50%] rounded-full radial mt-[-20%] mr-[-20%]"></div>
-                    <div className="absolute bottom-0 left-0 right-0 w-[100%] h-[45%] rounded-full bg-gradient-to-t from-purple via-purple/70 to-transparent rotate-[20deg]"></div>
-                  </div>
-                </ScrollParallax>
+                <div className="absolute top-0 left-0 w-[70vw] h-[130%] rounded-full bg-gradient-to-t from-orange via-yellow to-yellow translate-x-[-50vw] translate-y-[-15%] blur-xl flex items-center justify-center overflow-hidden">
+                  <div className="w-[80%] h-[50%] rounded-full radial mt-[-20%] mr-[-20%]"></div>
+                  <div className="absolute bottom-0 left-0 right-0 w-[100%] h-[45%] rounded-full bg-gradient-to-t from-purple via-purple/70 to-transparent rotate-[20deg]"></div>
+                </div>
               </MouseParallax>
             </div>
 
