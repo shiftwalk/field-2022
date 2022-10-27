953cd4122b804e340e035da2a4b0746e84b4a79b
Sam Goddard <hello@samgoddard.co.uk>
2022-10-27 15:41:16 +0100

    app init false

--- helpers/transitions.js
+++ helpers/transitions.js
@@ -4,11 +4,11 @@ export const fade = {
   },
   enter: { 
     opacity: 1,
-    transition: { duration: 0.75, ease: [0.83, 0, 0.17, 1] }
+    transition: { duration: 0.55, ease: [0.83, 0, 0.17, 1] }
   },
 	exit: {
     opacity: 0,
-		transition: { duration: 0.75, ease: [0.83, 0, 0.17, 1]  }
+		transition: { duration: 0.63, ease: [0.83, 0, 0.17, 1]  }
 	}
 }
 
@@ -20,37 +20,28 @@ export const blur = {
   enter: { 
     opacity: 1,
     filter: "blur(0px)",
-    transition: { duration: 1, ease: [0.83, 0, 0.17, 1] }
+    transition: { duration: 0.75, ease: [0.83, 0, 0.17, 1] }
   },
 	exit: {
     opacity: 0,
     filter: "blur(55px)",
-		transition: { duration: 0.75, ease: [0.83, 0, 0.17, 1] }
+		transition: { duration: 0.63, ease: [0.83, 0, 0.17, 1] }
 	}
 }
 
 export const blurScale = {
 	initial: { 
-    scale: 0.75
+    filter: "blur(200px)",
+    scale: 0.8
   },
   enter: { 
+    filter: "blur(0px)",
     scale: 1,
-    transition: { duration: 1.25, ease: [0.65, 0, 0.35, 1] }
+    transition: { duration: 1.2, ease: [0.33, 1, 0.68, 1] }
   },
 	exit: {
+    filter: "blur(200px)",
     scale: 0.9,
-		transition: { duration: 0.75, ease: [0.65, 0, 0.35, 1] }
-	}
-}
-
-export const reveal = {
-	initial: { y: '100%' },
-  enter: { 
-    y: 0,
-    transition: { duration: 0.65, ease: [0.83, 0, 0.17, 1] }
-  },
-	exit: {
-    y: '100%',
-		transition: { duration: 0.55, ease: [0.83, 0, 0.17, 1] }
+		transition: { duration: 0.63, ease: [0.65, 0, 0.35, 1] }
 	}
 }
