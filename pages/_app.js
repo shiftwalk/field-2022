fb557ac7622f27dcdfddfabdec63de6cd84d893c
Sam Goddard <hello@samgoddard.co.uk>
2022-10-27 17:38:29 +0100

    app init false

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,17 @@
 import '@/styles/main.css'
 import PreviewAlert from '@/components/preview-alert';
-import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-
   return (
     <>
       <DefaultSeo {...SEO} /> 
       
       { pageProps.preview && <PreviewAlert/>}
 
-      <AnimatePresence mode='wait' initial={false}>
-        <Component {...pageProps} key={router.asPath} />
-      </AnimatePresence>
+      <Component {...pageProps} />
     </>
   )
 }
