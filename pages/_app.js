6d0fc1ca6480780ea78b8ac4b874b8a59a99c186
Sam Goddard <hello@samgoddard.co.uk>
2022-10-27 14:20:23 +0100

    app init false

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
       <div className="border-r border-black fixed top-0 w-px left-3 md:left-4 lg:left-5 h-full z-10"></div>
       <div className="border-r border-black fixed top-0 w-px right-3 md:right-4 lg:right-5 h-full z-10"></div>
 
-      <AnimatePresence mode='wait'>
+      <AnimatePresence mode='wait' initial={false}>
         <Component {...pageProps} key={router.asPath} />
       </AnimatePresence>
     </>
