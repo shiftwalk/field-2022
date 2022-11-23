1c4d203505c6c628ba2c6d9742faba2fc56fb92b
Sam Goddard <hello@samgoddard.co.uk>
2022-11-23 14:32:26 +0000

    added gtag

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,23 @@ import PreviewAlert from '@/components/preview-alert';
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config';
+import * as gtag from '@/helpers/gtag'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+  }, [router.events])
+
   return (
     <>
       <DefaultSeo {...SEO} /> 
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '@/helpers/gtag'
 
 export default class MyDocument extends Document {
   render() {
@@ -12,6 +13,20 @@ export default class MyDocument extends Document {
           <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         
         <body>
